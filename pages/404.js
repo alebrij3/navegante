1d47d2aa7407b762581c6305e67b74589a491ec4
Alejandro Jimenez <ajf@fastmail.com>
2021-07-16 16:21:03 -0500

    UPDATE 404 page

--- pages/404.js
+++ pages/404.js
@@ -1,3 +1,9 @@
+import Layout from "../components/layout";
+
 export default function Custom404() {
-  return <h1>404 - Página no encontrada</h1>;
+  return (
+    <Layout>
+      <h1>404 - Página no encontrada</h1>
+    </Layout>
+  );
 }
