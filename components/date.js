87123690ef33a95285d7dfa2a6965ef11aada15c
Alejandro J <ajf@fastmail.com>
2021-10-15 13:19:24 -0500

    update date to spanish

--- components/date.js
+++ components/date.js
@@ -2,5 +2,9 @@ import { parseISO, format } from "date-fns";
 
 export default function Date({ dateString }) {
   const date = parseISO(dateString);
-  return <time dateTime={dateString}>{format(date, "LLLL d, yyyy")}</time>;
+  const months = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre']
+  const year = date.getFullYear();
+  const month = months[date.getMonth()].toLowerCase();
+  const day = date.getDate();
+  return <time dateTime={dateString}>{`${day} de ${month} ${year}`}</time>;
 }
